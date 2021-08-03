c7a848a152fd848c7d50ecade8f701ce2723e768
Robert I <robertorbbit@gmail.com>
2021-08-03 20:06:25 +0500

    Добавить метод для решения задачи 1885. Комфорт пассажиров

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Seminar2
             Console.WriteLine(GetSumm(16));
             var time = new Time(19, 44);            
             Console.WriteLine(time.GetAngleBetweenArrow());
+            Console.WriteLine(DiscomfortMaxMinTime(5000, 1200, 100, 1));
         }
 
         public static int GetSumm(int startNumber, int summ = 0)
@@ -21,8 +22,40 @@ namespace Seminar2
             else
                 return summ;
         }
+        public static (double, double) DiscomfortMaxMinTime(int height, int time, int maxSpeed, int comfortSpeed)
+        {
+            var remainingHeight = height;
+            var minDiscomfortTime = 0;
+            var maxDiscomfortTime = 0;
+            for (int t = time; t > 0; t-- )
+            {
+                if (t * maxSpeed > remainingHeight)
+                {
+                    remainingHeight = height - (time-t) * comfortSpeed;
+                }
+                else
+                {
+                    minDiscomfortTime = t;
+                    break;
+                }
+            }
 
-        
+            for (int t = time; t > 0; t--)
+            {
+                if (t * comfortSpeed > remainingHeight)
+                {
+                    remainingHeight = height - (time - t) * maxSpeed;
+                }
+                else
+                {
+                    maxDiscomfortTime = t;
+                    break;
+                }
+            }
+
+            return (minDiscomfortTime, maxDiscomfortTime);
+        }
+
     }
 
     public class Time
@@ -42,4 +75,5 @@ namespace Seminar2
             return Math.Abs(angle);
         }
     }
+
 }
