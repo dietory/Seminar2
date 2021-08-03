ed759921a468d6caf40cf72a5fe526e1f7c1c8b6
Robert I <robertorbbit@gmail.com>
2021-08-03 17:51:39 +0500

    Добавить метод для вычисления угла между стрелками часов

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Seminar2
         static void Main(string[] args)
         {
             Console.WriteLine(GetSumm(16));
+            var time = new Time(19, 44);            
+            Console.WriteLine(time.GetAngleBetweenArrow());
         }
 
         public static int GetSumm(int startNumber, int summ = 0)
@@ -18,6 +20,26 @@ namespace Seminar2
                 return GetSumm(nextNumber, summ);
             else
                 return summ;
+        }
+
+        
+    }
+
+    public class Time
+    {
+        int Hour;
+        int Minute;
+
+        public Time(int hour, int minute)
+        {
+            Hour = hour / 12 == 0? hour : hour - 12;
+            Minute = minute;
+        }
+
+        public int GetAngleBetweenArrow()
+        {
+            int angle = 6 * (5 * Hour - Minute);
+            return Math.Abs(angle);
         }
     }
 }
